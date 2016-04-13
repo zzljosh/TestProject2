ee86c699a858a83f00682b00f62d22e2f1edd189
zhz256 <zhz256@ucsd.edu>
2016-04-13 11:41:43 -0700

    Bug fixed

---
+++ app/src/androidTest/java/com/example/zhz256/testproject/tests/JUnit_test.java
@@ -0,0 +1,31 @@
+package com.example.zhz256.testproject.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.zhz256.testproject.MainActivity;
+import com.example.zhz256.testproject.R;
+
+import dalvik.annotation.TestTargetClass;
+
+/**ls
+ * Created by zhz256 on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2 <MainActivity> {
+    MainActivity mainActivity;
+    public JUnit_test(){
+        super(MainActivity.class);
+    }
+
+    public void test_first(){
+        mainActivity = getActivity();
+
+        TextView textView = (TextView)mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye World", tester);
+    }
+
+
+
+}
